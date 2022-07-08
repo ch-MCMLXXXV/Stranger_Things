011ab343f86916bc644d39ae134ba5f598a7dd16
Courtney Harris <cjharris1985@gmail.com>
2022-07-07 21:13:22 -0500

    made changes to fetch post request

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+// export const cohortName = '2204-FTB-ET-WEB-PT';
+// export const APIURL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
+
+/*  export const fetchAllPosts = async () => {
+    fetch(`${APIURL}/posts`)
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+        })
+        .catch(console.error);
+};
+
+fetchAllPosts();*/
--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+const cohortName = '2204-FTB-ET-WEB-PT';
+const APIURL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
+
+const App = () => {
+    const [posts, setPosts] = useState([]);
+    console.log(posts);
+    useEffect(() => {
+        const fetchAllPosts = async () => {
+            const response = await fetch(`${APIURL}/posts`);
+            const result = await response.json();
+            setPosts(result.data.posts);
+        };
+        fetchAllPosts();
+    }, []);
+    return (
+        <>
+            <div>
+                <header>
+                    <h1 className="">Stranger Things</h1>
+                </header>
+            </div>
+            <h1>Posts</h1>
+            {posts.map((post) => (
+                <div key={post._id}>
+                    <h3>{post.title}</h3>
+                    <div>{post.description}</div>
+                </div>
+            ))}
+        </>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
