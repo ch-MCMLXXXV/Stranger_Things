981bb6c152e46bd21f7523de14668cdc4dfe75e0
Courtney Harris <cjharris1985@gmail.com>
2022-07-07 21:06:29 -0500

    Commented out code; moved to src/index.js q git push quit q clear git status ls

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-import React from 'react';
+/* import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => <div>This Will Be Stranger Things</div>;
+const cohortName = '2204-FTB-ET-WEB-PT';
+const APIURL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
 
-export default App;
+const App = () => {
+    const [posts, setPosts] = useState([]);
+    console.log(posts);
+    useEffect(() => {
+        const fetchAllPosts = async () => {
+            const response = await fetch(`${APIURL}/posts`);
+            const data = await response.json();
+            setPosts(data);
+            console.log(data);
+        };
+        fetchAllPosts();
+    }, []);
+    return (
+        <>
+            <h1>Posts</h1>
+            {posts.map((post) => (
+                <div key={post._id}>
+                    <h3>{post.title}</h3>
+                    <div>{post.description}</div>
+                </div>
+            ))}
+        </>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('app')); */
