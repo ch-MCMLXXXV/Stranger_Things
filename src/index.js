42427b36a2715ac76f228bb08ab36d0cadee9e87
Courtney Harris <cjharris1985@gmail.com>
2022-07-09 20:31:57 -0500

    merged import from Create & Update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { Create, Update } from './components';
 
-const cohortName = '2204-FTB-ET-WEB-PT';
-const APIURL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
+export const cohortName = '2204-FTB-ET-WEB-PT';
+export const APIURL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
 
 const App = () => {
     const [posts, setPosts] = useState([]);
+    const [postId, setPostId] = useState(null);
     console.log(posts);
     useEffect(() => {
         const fetchAllPosts = async () => {
@@ -23,10 +25,27 @@ const App = () => {
                 </header>
             </div>
             <h1>Posts</h1>
+            {postId ? (
+                <Update
+                    posts={posts}
+                    setPosts={setPosts}
+                    postId={postId}
+                    setPostId={setPostId}
+                />
+            ) : (
+                <Create posts={posts} setPosts={setPosts} />
+            )}
+
             {posts.map((post) => (
                 <div key={post._id}>
                     <h3>{post.title}</h3>
                     <div>{post.description}</div>
+                    <div>{post.location}</div>
+                    <div>{post.price}</div>
+                    <div>{post.willDeliver}</div>
+                    <button type="button" onClick={() => setPostId(post._id)}>
+                        Edit
+                    </button>
                 </div>
             ))}
         </>
