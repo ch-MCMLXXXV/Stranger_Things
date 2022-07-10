26197d8cd3cdd1c1c64a990b461cb7c1ba91b91b
Courtney Harris <cjharris1985@gmail.com>
2022-07-10 18:55:29 -0500

    merged code for account reg

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,1 +1,64 @@
 import React, { useState } from 'react';
+import { APIURL } from '../index';
+
+const Register = ({ setToken }) => {
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   const [confirmPassword, setConfirmPassword] = useState('');
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      const response = await fetch(`${APIURL}/users/register`, {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({
+            user: {
+               username: '',
+               password: '',
+            },
+         }),
+      });
+      const result = await response.json();
+      setToken(result);
+   };
+
+   const onConfirmPassword = () => {
+      if (password === true && confirmPassword === true) {
+         if (password !== confirmPassword) {
+            console.log('The passwords do not match');
+            return;
+         } else {
+            console.log('Ok');
+         }
+      }
+   };
+
+   return (
+      <>
+         <h2>Create an Account</h2>
+         <form onsubmit={handleSubmit}>
+            <input
+               type='text'
+               placeholder='Enter username'
+               value={username}
+               onChange={(e) => setUsername(e.target.value)}></input>
+            <input
+               type='password'
+               placeholder='Enter password'
+               value={password}
+               onChange={(e) => setPassword(e.target.value)}></input>
+            <input
+               type='password'
+               placeholder='Confirm password'
+               value={confirmPassword}
+               onChange={(e) => setConfirmPassword(e.target.value)}
+               onBlur={onConfirmPassword}></input>
+            <button type='submit'>Register</button>
+         </form>
+      </>
+   );
+};
+
+export default Register;
