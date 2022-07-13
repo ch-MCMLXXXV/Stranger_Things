e875ce0daef1973ad8693910b9825fddaf58aaf4
Courtney Harris <cjharris1985@gmail.com>
2022-07-13 13:17:54 -0500

    fixed Routes bug

--- src/App.js
+++ src/App.js
@@ -1,43 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import { APIURL } from './index';
-import { Link, Outlet } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+// import { APIURL } from './index';
+// import { Link, Outlet } from 'react-router-dom';
 
-export default function App() {
-   const [posts, setPosts] = useState([]);
+// export default function App() {
+//    const [posts, setPosts] = useState([]);
 
-   useEffect(() => {
-      const fetchAllPosts = async () => {
-         const response = await fetch(`${APIURL}/posts`);
-         const result = await response.json();
-         setPosts(result.data.posts);
-      };
-      fetchAllPosts();
-   }, []);
+//    useEffect(() => {
+//       const fetchAllPosts = async () => {
+//          const response = await fetch(`${APIURL}/posts`);
+//          const result = await response.json();
+//          setPosts(result.data.posts);
+//       };
+//       fetchAllPosts();
+//    }, []);
 
-   return (
-      <>
-         <div>
-            <header>
-               <h1 className=''>Stranger's Things</h1>
-            </header>
-         </div>
-         <div>
-            <h1>Posts</h1>
-            <nav>
-               <Link to='/components/Register'>Register</Link> | {''}
-               <Link to='/components/Login'>Login</Link>
-            </nav>
-            <Outlet />
-         </div>
-         {posts.map((post) => (
-            <div key={post._id}>
-               <h3>{post.title}</h3>
-               <div>{post.description}</div>
-               <div>{post.location}</div>
-               <div>{post.price}</div>
-               <div>{post.willDeliver}</div>
-            </div>
-         ))}
-      </>
-   );
-}
+//    return (
+//       <>
+//          <div>
+//             <header>
+//                <h1 className=''>Stranger's Things</h1>
+//             </header>
+//          </div>
+//          <div>
+//             <h1>Posts</h1>
+//             <nav>
+//                <Link to='/Register'>Register</Link> | {''}
+//                <Link to='/Login'>Login</Link>
+//             </nav>
+//             <Outlet />
+//          </div>
+//          {posts.map((post) => (
+//             <div key={post._id}>
+//                <h3>{post.title}</h3>
+//                <div>{post.description}</div>
+//                <div>{post.location}</div>
+//                <div>{post.price}</div>
+//                <div>{post.willDeliver}</div>
+//             </div>
+//          ))}
+//       </>
+//    );
+// }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Login, Register } from './components';
+import { Login, Register, Landing } from './components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const cohortName = '2204-FTB-ET-WEB-PT';
 export const APIURL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
 
+const Root = () => {
+   const [token, setToken] = useState('');
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<Landing />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+            <Route
+               path='login'
+               element={<Login token={token} setToken={setToken} />}></Route>
+            <Route
+               path='*'
+               element={
+                  <main>
+                     <p>There's nothing here!</p>
+                  </main>
+               }></Route>
+         </Routes>
+      </BrowserRouter>
+   );
+};
 const root = ReactDOM.createRoot(document.getElementById('app'));
-root.render(
-   <BrowserRouter>
-      <Routes>
-         <Route path='/' element={<App />}>
-            <Route path='register' element={<Register />} />
-            <Route path='login' element={<Login />} />
-         </Route>
-      </Routes>
-   </BrowserRouter>
-);
+root.render(<Root />);
